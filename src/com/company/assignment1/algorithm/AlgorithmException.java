c85859ed70ec2c13ed2e4c2853f21b477f68ec3b
S1mpler <stas.dior@gmail.com>
2017-12-03 16:15:29 +0100

    Update

--- src/com/company/assignment1/algorithm/AlgorithmException.java
+++ src/com/company/assignment1/algorithm/AlgorithmException.java
@@ -1,5 +1,9 @@
 package com.company.assignment1.algorithm;
 
+/**
+ * Algorithm exception.
+ * Created by s1mpler on 11/24/17.
+ */
 public class AlgorithmException extends Exception {
     public AlgorithmException(String message) {
         super(message);
